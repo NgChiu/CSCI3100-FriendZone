5dcc628cc2f4a871ca3b811995dc66bfb6cac159
emilyelhy <49402353+emilyelhy@users.noreply.github.com>
2020-03-27 23:35:40 +0800

    Update PostDetail.js
    Comment Added

--- app/server/PostDetail.js
+++ app/server/PostDetail.js
@@ -1,1 +1,4 @@
 //Show the Post Detail after the user clicked the post in the categories
+//Redirect from category pages
+//Have button to redirect user back to category page
+//Have button to redirect user to join post page
