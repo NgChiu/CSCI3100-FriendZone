1cca189deb8c50c5f94c54158f0f64f8cdebedbe
kwokben123 <49425775+kwokben123@users.noreply.github.com>
2020-04-22 21:02:56 +0800

    Update Logout.js

--- app/public/css/js/Logout.js
+++ app/public/css/js/Logout.js
@@ -1,10 +1,9 @@
-$(document).ready(function () {
+$(function() {
     $("#logoutbtn").click(function() {
         if (localStorage.hasOwnProperty("token") === false)
             alert("you didn't log in yet!");
         else
             localStorage.removeItem("token");
-        window.location.href = "Login.html";
+        location.replace("Login.html");
     })
 });
-
