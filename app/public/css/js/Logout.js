0176fd2a4ab7db7ac1ea9f00d518a36506258b6f
kwokben123 <49425775+kwokben123@users.noreply.github.com>
2020-04-22 20:13:27 +0800

    Update Logout.js

--- app/public/css/js/Logout.js
+++ app/public/css/js/Logout.js
@@ -1,6 +1,5 @@
 $(document).ready(function () {
     $("#logoutbtn").click(function() {
-        alert("you try to log out!");
         if (localStorage.hasOwnProperty("token") === false)
             alert("you didn't log in yet!");
         else
