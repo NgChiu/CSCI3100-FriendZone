42756b50e9cb7b0c0d7f99eabe651a8fdc75f9de
NgChiu <49191324+NgChiu@users.noreply.github.com>
2020-04-23 23:23:04 +0800

    Update Event_parts.js

--- app/public/css/js/Event_parts.js
+++ app/public/css/js/Event_parts.js
@@ -1,5 +1,26 @@
 //
 
+//join event function
+function JoinEvent(inputbtn){
+	var post_ID = inputbtn.id;
+	//alert("The post id is "+ post_ID);
+	$.ajax({
+			url: "http://localhost:3000/catalog/post/join",
+			type: "POST",
+			dataType: "json",
+			data:{
+				postID: post_ID,
+				token: localStorage.getItem("token")
+			},
+			success: function(response){
+				alert("You have joined the event!");
+			},
+			error: function(response){
+				alert("Fail to join!");
+			}
+	});
+}
+
 //Display event--------------------------------------------------
 $("#update_btn").click(function(){
 	alert('Events Updated!');
@@ -8,7 +29,7 @@ $("#update_btn").click(function(){
 	if (x === ""){
 		$.getJSON("http://localhost:3000/catalog/post",function(data) {
 			for (var i = 0; i < data.length ; i++) {
-				var input = $('<input type="button" name="join_event" id="' + data[i]._id + '" value="Join" class="mdl-button mdl-js-button mdl-button--raised" style="background-color: #48AAAD; float: right;">');
+				var input = $('<input type="button" name="join_event" id="' + data[i]._id + '" value="Join" onclick="JoinEvent(this)" class="mdl-button mdl-js-button mdl-button--raised" style="background-color: #48AAAD; float: right;">');
 				$("#post_display").prepend("<h2>" + data[i].Title + "</h2> <p>Category: " + data[i].Category + "</p> <p>Date:" + data[i].Date + " </p> <p>Venue: " + data[i].Venue + "</p> <p>Quota: " + data[i].Quota + "</p> <p>Number of participant(s): " + data[i].NumberOfParticipants + "</p><p>Detail: " + data[i].Content + "</p><hr>");
 				$("#post_display").prepend(input);
 			}
@@ -17,36 +38,13 @@ $("#update_btn").click(function(){
 	else{
 		$.getJSON("http://localhost:3000/catalog/post/"+x,function(data) {
 			for (var i = 0; i < data.length ; i++) {
-				var input = $('<input type="button" name="join_event" id="' + data[i]._id + '" value="Join" class="mdl-button mdl-js-button mdl-button--raised" style="background-color: #48AAAD; float: right;">');
+				var input = $('<input type="button" name="join_event" id="' + data[i]._id + '" value="Join" onclick="JoinEvent(this)" class="mdl-button mdl-js-button mdl-button--raised" style="background-color: #48AAAD; float: right;">');
 				$("#post_display").prepend("<h2>" + data[i].Title + "</h2> <p>Category: " + data[i].Category + "</p> <p>Date:" + data[i].Date + " </p> <p>Venue: " + data[i].Venue + "</p> <p>Quota: " + data[i].Quota + "</p> <p>Number of participant(s): " + data[i].NumberOfParticipants + "</p><p>Detail: " + data[i].Content + "</p><hr>");
 				$("#post_display").prepend(input);
 			}
 		});
 	}
 	
-	
-	//join event
-	$("input[name= 'join_event']").click(function (){
-
-		var post_ID = this.id;
-		alert("The post id is "+ post_ID);
-
-
-		$.ajax({
-			url: "http://localhost:3000/catalog/post/join",
-			type: "POST",
-			dataType: "json",
-			data:{
-				postID: post_ID,
-				token: localStorage.getItem("token")
-			}
-			success: function(response){
-				alert("Joined!");
-			}
-		});
-
-
-	});
 });
 
 
@@ -84,8 +82,11 @@ $( "#create" ).submit( function (){
 		    token: localStorage.getItem("token")
 
                 },
-                success: function(data){
+                success: function(response){
                     alert("Event Created!");
-                }
+                },
+		error: function(response){
+		    alert("Fail to create!");	
+		}
             })
 });
