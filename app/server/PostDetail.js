d63e5b71b7e3f55ac87b9787901d19a51834d7d6
kwokben123 <49425775+kwokben123@users.noreply.github.com>
2020-04-23 23:48:06 +0800

    Delete PostDetail.js

--- app/server/PostDetail.js
+++
@@ -1,4 +0,0 @@
-//Show the Post Detail after the user clicked the post in the categories
-//Redirect from category pages
-//Have button to redirect user back to category page
-//Have button to redirect user to join post page
