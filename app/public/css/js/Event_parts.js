86cfcc393e4ad8c1c990c009bb068c07d2ce8216
NgChiu <49191324+NgChiu@users.noreply.github.com>
2020-04-23 12:42:32 +0800

    Update Event_parts.js

--- app/public/css/js/Event_parts.js
+++ app/public/css/js/Event_parts.js
@@ -46,7 +46,7 @@ $("#update_btn").click(function(){
     			success: function(data){
     				$.getJSON("/post/join/"+ post_ID, function(host){
     					alert("Joined!");
-    					alert("Please contact the host vai Line: "+ host.LineID);
+    					alert("Please contact the host via Line: "+ host.LineID);
     				});
     				$.getJSON("member/myself", function(member){
     					$.ajax({
