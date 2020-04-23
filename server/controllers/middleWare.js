b9ae46d5f5e51da2ece523982fda9e581d885e00
emilyelhy <49402353+emilyelhy@users.noreply.github.com>
2020-04-23 21:59:23 +0800

    Update middleWare.js

--- server/controllers/middleWare.js
+++ server/controllers/middleWare.js
@@ -3,18 +3,14 @@
 var jwt  = require ('jsonwebtoken');
 // var config = require ('../config');
 
-const { JWT_SECRET } = {
+const  JWT_SECRET  = {
   "mongoURI": "mongodb+srv://FriendZone:csci3100@cluster0-si1k7.mongodb.net/test",
   "jwtSecret": "SecretKey"
 };
 
 module.exports =  (req, res, next) => {
-  console.log("Middleware");
-  const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVlOWMwYzRhZTk5MmM1MGRiYjgzMDQwOSIsImlhdCI6MTU4NzU2OTI3OSwiZXhwIjoxNTg3OTI5Mjc5fQ.AL-yADdkyAaWkKHqtElAzkrVDa5jqYDWY4D0aGrX4Ug";
-  // const token = req.header('x-auth-token');
-  // console.log(req.localStorage.getItem('token'));
-  // var token = JSON.parse(localStorage.getItem('token'));
-  console.log(req.header);
+  const token = req.body.token;  
+  console.log(token);
 
   //If no token
   if (!token){
@@ -24,13 +20,13 @@ module.exports =  (req, res, next) => {
 
   try {
     //Check if token valid
-    console.log("Before verify");
     const decoded = jwt.verify(token, JWT_SECRET.jwtSecret);
+    if(!decoded) throw Error('could not decode token');
     //return user
     req.user = decoded;
-    console.log("After verify");
     next();
   } catch (e) {
     res.status(400).json({ msg: 'Invalid Access (INVALID Token)' });
+    console.log(e.message);
   }
 };
