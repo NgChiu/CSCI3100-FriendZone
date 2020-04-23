d753b4d7b18410ebde03619a3ad675db4356468a
emilyelhy <49402353+emilyelhy@users.noreply.github.com>
2020-04-23 22:03:13 +0800

    Update and rename Category.js to Joined.js

--- server/models/Joined.js
+++ server/models/Joined.js
@@ -3,10 +3,10 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const model = mongoose.model;
 
-const CategorySchema = new Schema({
+const JoinedSchema = new Schema({
 	MemberID: { type: mongoose.Schema.Types.ObjectId, required: true, ref: 'members' },
 	PostID: { type: mongoose.Schema.Types.ObjectId, required: true, ref: 'posts' }
 });
 
 //Export model
-module.exports = model('category', CategorySchema);
+module.exports = model('joined', JoinedSchema);
