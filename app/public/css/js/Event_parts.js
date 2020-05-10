10be3e0e4f83b6d88cb2fd9bffc0713ecf66b50d
emilyelhy <49402353+emilyelhy@users.noreply.github.com>
2020-05-10 18:05:29 +0800

    Update Event_parts.js

--- app/public/css/js/Event_parts.js
+++ app/public/css/js/Event_parts.js
@@ -29,7 +29,7 @@ $("#update_btn").click(function(){
 	if (x === ""){
 		var j = 0;
 		$.getJSON("http://localhost:3000/catalog/post",function(data) {
-			for (var i = 0; i < data.length ; i++) {
+			for (var i = 0; i < data.posts.length ; i++) {
 				var input = $('<input type="button" name="join_event" id="' + data.posts[i]._id + '" value="Join" onclick="JoinEvent(this)" class="mdl-button mdl-js-button mdl-button--raised" style="background-color: #48AAAD; float: right;">');
 				$("#post_display").prepend("<hr>");
 				for (; j < data.post[i].NumberOfParticipants ; j++)
