909ab19f095104d62bb9467e677676c106212f5d
NgChiu <49191324+NgChiu@users.noreply.github.com>
2020-05-12 14:12:40 +0800

    Update Event_parts.js

--- app/public/css/js/Event_parts.js
+++ app/public/css/js/Event_parts.js
@@ -22,12 +22,12 @@ function JoinEvent(inputbtn){
 	});
 }
 
-  //show host
+  //show host information
 function ShowHostInfo(id){
   document.getElementById("ShowHost"+id).classList.toggle("show");
 }
 
-  //show participant
+  //show participant information
 function ShowPartiInfo(id){
   document.getElementById("ShowParti"+id).classList.toggle("show");
 }
@@ -40,6 +40,7 @@ $("#update_btn").click(function(){
 	if (x === ""){
 		var ppl = 0;
 		$.getJSON("http://localhost:3000/catalog/post",function(data) {
+		//if there is no post
       		if (data.posts.length == 0)
           		$("#post_display").append("<strong>No post is available yet.</strong>");
         	var cnt_mod = data.posts.length % 2;
@@ -47,6 +48,7 @@ $("#update_btn").click(function(){
         	var PartiInfo_ID = 0;
         	var HostInfo_ID = 0;
         	var i = 0;
+		//since each row will contain 2 posts, if there is an odd number posts, we will make the row only contain 1 post 
         	if (cnt_mod == 1){
 
           		$("#post_display").prepend('<div class="row"><div class="col-sm-6"><div class="mdl-shadow--2dp card"><div class="card-body"><h5 class="card-title">' + data.posts[i].Title + '</h5><p class="card-text">Category: '+ data.posts[i].Category +'</p><p class="card-text">Date: '+ data.posts[i].Date +'</p><p class="card-text">Venue: '+ data.posts[i].Venue +'</p><p class="card-text">Quota: '+ data.posts[i].Quota +'</p><p class="card-text">Number of participant(s): '+ data.posts[i].NumberOfParticipants +'</p><p class="card-text">Detail: '+ data.posts[i].Content +'</p><div class="dropdown"><button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect dropbtn" onclick="ShowHostInfo('+HostInfo_ID+')" style="margin:10px;">Show Host</button><div id="ShowHost'+HostInfo_ID+'" class="dropdown-content"><a>UserID: '+ data.HostIDList[i] +' [RP mark: '+ data.HostMarkList[i] +']</a></div></div><div class="dropdown"><button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect dropbtn" onclick="ShowPartiInfo('+PartiInfo_ID+')" style="margin:10px;">Show Participants</button><div id="ShowParti'+PartiInfo_ID+'" class="dropdown-content"></div></div><div class="mdl-card__actions mdl-card--border"><a id="' + data.posts[i]._id + '"class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect" onclick="JoinEvent(this)">Join</a></div></div></div></div></div><br>');
@@ -60,7 +62,7 @@ $("#update_btn").click(function(){
           		i++;
         	}
 
-
+//      display a row with 2 posts
         	for (var z = 0; z < cnt_set; z++){
           		$("#post_display").prepend('<div class="row"><div class="col-sm-6"><div class="mdl-shadow--2dp card"><div class="card-body"><h5 class="card-title">' + data.posts[i].Title + '</h5><p class="card-text">Category: '+ data.posts[i].Category +'</p><p class="card-text">Date: '+ data.posts[i].Date +'</p><p class="card-text">Venue: '+ data.posts[i].Venue +'</p><p class="card-text">Quota: '+ data.posts[i].Quota +'</p><p class="card-text">Number of participant(s): '+ data.posts[i].NumberOfParticipants +'</p><p class="card-text">Detail: '+ data.posts[i].Content +'</p><div class="dropdown"><button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect dropbtn" onclick="ShowHostInfo('+HostInfo_ID+')" style="margin:10px;">Show Host</button><div id="ShowHost'+HostInfo_ID+'" class="dropdown-content"><a>UserID: '+ data.HostIDList[i] +' [RP mark: '+ data.HostMarkList[i] +']</a></div></div><div class="dropdown"><button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect dropbtn" onclick="ShowPartiInfo('+PartiInfo_ID+')" style="margin:10px;">Show Participants</button><div id="ShowParti'+PartiInfo_ID+'" class="dropdown-content"></div></div><div class="mdl-card__actions mdl-card--border"><a id="' + data.posts[i]._id + '"class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect" onclick="JoinEvent(this)">Join</a></div></div></div></div><div class="col-sm-6"><div class="mdl-shadow--2dp card"><div class="card-body"><h5 class="card-title">' + data.posts[i+1].Title + '</h5><p class="card-text">Category: '+ data.posts[i+1].Category +'</p><p class="card-text">Date: '+ data.posts[i+1].Date +'</p><p class="card-text">Venue: '+ data.posts[i+1].Venue +'</p><p class="card-text">Quota: '+ data.posts[i+1].Quota +'</p><p class="card-text">Number of participant(s): '+ data.posts[i+1].NumberOfParticipants +'</p><p class="card-text">Detail: '+ data.posts[i+1].Content +'</p><div class="dropdown"><button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect dropbtn" onclick="ShowHostInfo('+(HostInfo_ID+1)+')" style="margin:10px;">Show Host</button><div id="ShowHost'+(HostInfo_ID+1)+'" class="dropdown-content"><a>UserID: '+ data.HostIDList[i+1] +' [RP mark: '+ data.HostMarkList[i+1] +']</a></div></div><div class="dropdown"><button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect dropbtn" onclick="ShowPartiInfo('+(PartiInfo_ID+1)+')" style="margin:10px;">Show Participants</button><div id="ShowParti'+(PartiInfo_ID+1)+'" class="dropdown-content"></div></div><div class="mdl-card__actions mdl-card--border"><a id="' + data.posts[i+1]._id + '"class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect" onclick="JoinEvent(this)">Join</a></div></div></div></div></div><br>');
           		
@@ -80,7 +82,7 @@ $("#update_btn").click(function(){
         	}
     	});
 	}
-	else{
+	else{ 		//if user select the specific category to display
 		var ppl = 0;
 		$.getJSON("http://localhost:3000/catalog/post/"+x,function(data) {
 			if (data.postList.length == 0)
@@ -140,9 +142,6 @@ $( "#create" ).submit( function (){
     var quota = $.trim($('#quota').val());
     var content = $.trim($('#describe').val());
     var line_ID;
-            // $.getJSON("http://localhost:3000/catalog/member/myself", function(member){
-            //     line_ID = member.LineID;
-            //  })
 
             $.ajax({
                 url: 'http://localhost:3000/catalog/post/create',
