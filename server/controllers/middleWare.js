3a7194b70b559fd6f378575a2acca50d45c8620c
emilyelhy <49402353+emilyelhy@users.noreply.github.com>
2020-05-17 01:34:04 +0800

    Added Comment

--- server/controllers/middleWare.js
+++ server/controllers/middleWare.js
@@ -1,32 +1,36 @@
-//var async = require('async');
-
+/****************************************************************
+ * Purpose  Get current member's _id from token
+ * Author   LAW Hei Yiu, LEUNG Pok Ho
+ * Date     2020-05-16
+ ***************************************************************/
 var jwt  = require ('jsonwebtoken');
-// var config = require ('../config');
 
 const  JWT_SECRET  = {
-  "mongoURI": "mongodb+srv://FriendZone:csci3100@cluster0-si1k7.mongodb.net/test",
-  "jwtSecret": "SecretKey"
+  	"mongoURI": "mongodb+srv://FriendZone:csci3100@cluster0-si1k7.mongodb.net/test",
+  	"jwtSecret": "SecretKey"
 };
 
+/****************************************************************
+ * Purpose  Get current member's _id from token
+ ***************************************************************/
 module.exports =  (req, res, next) => {
-  const token = req.body.token;  
-  // console.log(token);
+  	const token = req.body.token;
 
   //If no token
-  if (!token){
-  	console.log("NO token");
-    return res.status(401).json({ msg: 'Invalid Access (NO Token)' });
-}
+	if (!token){
+  		console.log("NO token");
+    	return res.status(401).json({ msg: 'Invalid Access (NO Token)' });
+	}
 
-  try {
-    //Check if token valid
-    const decoded = jwt.verify(token, JWT_SECRET.jwtSecret);
-    if(!decoded) throw Error('could not decode token');
-    //return user
-    req.user = decoded;
-    next();
-  } catch (e) {
-    res.status(400).json({ msg: 'Invalid Access (INVALID Token)' });
-    console.log(e.message);
-  }
+  	try {
+    	//Check if token valid
+    	const decoded = jwt.verify(token, JWT_SECRET.jwtSecret);
+    	if(!decoded) throw Error('could not decode token');
+   		//return user
+    	req.user = decoded;
+    	next();
+  	} catch (e) {
+    	console.log(e.message);
+    	res.status(400).json({ msg: 'Invalid Access (INVALID Token)' });
+  	}
 };
