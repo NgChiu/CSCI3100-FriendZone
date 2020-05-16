61970937644d17120f77cd7a825d10c088cacbaf
emilyelhy <49402353+emilyelhy@users.noreply.github.com>
2020-05-17 01:43:10 +0800

    Added Comment

--- server/models/Genre.js
+++ server/models/Genre.js
@@ -1,6 +1,6 @@
 /****************************************************************
  * Purpose  Schema for marking post(event) with their category
- *			For easier to display posts(events) by category
+ *	    For easier to display posts(events) by category
  * Author   LAW Hei Yiu, LEUNG Pok Ho
  * Date     2020-05-16
  ***************************************************************/
