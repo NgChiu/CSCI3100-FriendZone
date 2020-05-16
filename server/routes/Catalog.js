669df7f9274a00bcb358dbe17881a8c0d75fd36c
emilyelhy <49402353+emilyelhy@users.noreply.github.com>
2020-05-17 01:46:17 +0800

    Added Comment

--- server/routes/Catalog.js
+++ server/routes/Catalog.js
@@ -1,3 +1,8 @@
+/****************************************************************
+ * Purpose  Set routes for different function in controllers folder
+ * Author   LAW Hei Yiu, LEUNG Pok Ho
+ * Date     2020-05-16
+ ***************************************************************/
 var express = require('express');
 var router = express.Router();
 var auth = require('../controllers/middleWare');
@@ -6,7 +11,9 @@ var auth = require('../controllers/middleWare');
 var postController = require('../controllers/postController');
 var memberController = require('../controllers/memberController');
 
-
+/****************************************************************
+ * Routes related to post(event)
+ ***************************************************************/
 router.get('/post', postController.post_list);
 
 router.post('/post/create', auth, postController.post_create);
@@ -18,7 +25,9 @@ router.post('/post/delete', auth, postController.post_delete);
 router.post('/post/quit', auth, postController.post_quit);
 
 router.get('/post/:catID', postController.show_category);
-
+/****************************************************************
+ * Routes related to member
+ ***************************************************************/
 router.post('/member/login', memberController.member_login);
 
 router.post('/member/register',memberController.member_register);
@@ -27,4 +36,9 @@ router.post('/member/myself',auth, memberController.myself);
 
 router.post('/member/report', auth, memberController.report_user);
 
+router.post('/member/edit', auth, memberController.change_pw);
+
+/****************************************************************
+ * Export
+ ***************************************************************/
 module.exports = router;
