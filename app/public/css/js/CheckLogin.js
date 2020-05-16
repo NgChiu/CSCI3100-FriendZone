fb9b188ed78eb3845be3ee40a6eb2ea60a936785
kwokben123 <49425775+kwokben123@users.noreply.github.com>
2020-05-16 17:44:36 +0800

    Add comments

--- app/public/css/js/CheckLogin.js
+++ app/public/css/js/CheckLogin.js
@@ -1,6 +1,6 @@
-$(function() {
-    if (localStorage.hasOwnProperty("token") === false && location.href !== "Login.html") {
-        alert("Please sign in first.");
-        location.replace("Login.html");
+$(function() { //when any pages starts except login page and register page, check whether the user has logged in or not 
+    if (localStorage.hasOwnProperty("token") === false) { // if localstorage has no token, implies haven't logged in yet
+        alert("Please sign in first.");  //tells user to login forst
+        location.replace("Login.html"); // redirect users to login page
     }
 });
