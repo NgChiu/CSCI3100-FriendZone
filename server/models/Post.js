a4182b68404e3f8e5c33416d37a1c64207f3166a
emilyelhy <49402353+emilyelhy@users.noreply.github.com>
2020-05-17 01:45:14 +0800

    Added Comment

--- server/models/Post.js
+++ server/models/Post.js
@@ -1,9 +1,13 @@
+/****************************************************************
+ * Purpose  Schema for posts(events)
+ * Author   LAW Hei Yiu, LEUNG Pok Ho
+ * Date     2020-05-16
+ ***************************************************************/
 const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 const model = mongoose.model;
 
-//del jor warning and tags
 //StartTime, EndTime in format HHMM, e.g. "2130", "2030"
 //Date in format YYYYMMDD, e.g. "20200418"
 const PostSchema = new Schema({
@@ -15,10 +19,11 @@ const PostSchema = new Schema({
 	EndTime: { type: String, required: true },
 	Date: { type: String, required: true },
 	Venue: { type: String, required: true },
-	//JoinedUsername: { type: String },
 	NumberOfParticipants: { type: Number, required: true },
 	LineID: { type: String, required: true }
 });
 
-//Export model
+/****************************************************************
+ * Export model
+ ***************************************************************/
 module.exports = model('post', PostSchema);
